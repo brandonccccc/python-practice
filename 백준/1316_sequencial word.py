976c3a90958ebd7b815da6328f6623c9c6e36f61
brandonccccc <byeongjin180@gmail.com>
2021-08-01 13:54:54 +0900

    1316 seq word re study

--- 백준/1316_sequencial word.py
+++ 백준/1316_sequencial word.py
@@ -1,9 +1,14 @@
 count_word = 0
 count = 0
-N = int(input())
-
+#N = int(input())
 word = list(input())
+seq_word = []
 
+for i in range(0, len(word)):
+    if i < int(len(word)):
+        if word[i] != word[i+1]:
+            seq_word.append(word[i])
+    else:
+        seq_word.append(word[i])
 
-
-
+print(seq_word)
