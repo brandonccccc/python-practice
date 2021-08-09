94e665b1733bd5054a7da4999d0b987f5c3999dc
brandonccccc <byeongjin180@gmail.com>
2021-08-09 16:39:06 +0900

    2445 solving

---
+++ 백준/2445_star8.py
@@ -0,0 +1,17 @@
+N = int(input())
+count = 0
+
+
+
+
+"""
+for i in range(0,N):
+    print("*"*count)
+    count -= 1
+    
+
+for i in range (1, N+1):
+    print("*"*i)
+    
+for i in range (1, N+1):
+    print(str("*"*i).rjust(N))"""
