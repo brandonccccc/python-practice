96bd991f9b63f0fb7ec98e24dac9b1ff2f09ded9
brandonccccc <byeongjin180@gmail.com>
2021-08-18 16:30:30 +0900

    add 2133 9461 solving 2225

---
+++ 백준/2225_divis sum.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+num = [i for i in range(N+1)]
+dp = []
+print(num)
+
+for i in range(K):
+    
+
+
+print(ans%1000000000)
---
+++ 백준/9461_파도반수열.py
@@ -0,0 +1,11 @@
+T = int(input())
+for _ in range(T):
+    dp = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
+    N = int(input())
+    if N < 10:
+        print(dp[N-1])
+    else:
+        for i in range(10, N):
+            dp.append(dp[i-1] + dp[i-5])
+        print(dp)
+        print(dp[N-1])
