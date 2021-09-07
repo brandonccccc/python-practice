8fea34a1655855ad1b825f5a5cb5c61e9ccc8591
brandonccccc <byeongjin180@gmail.com>
2021-09-07 16:57:34 +0900

    solving

--- 백준/1406_에디터.py
+++ 백준/1406_에디터.py
@@ -1,8 +1,28 @@
 import sys
-S = sys.stdin.readline()
+S = list(sys.stdin.readline().rstrip())
 N = int(sys.stdin.readline())
-
+cs = len(S)
 for _ in range(N):
     command = sys.stdin.readline().split()
-    
-    
+    if command[0] == 'D':
+        if cs == len(S):
+            cs = cs
+        else:
+            cs += 1
+    if command[0] == 'L':
+        if cs == 0:
+            cs = cs
+        else:
+            cs -= 1
+    if command[0] == 'B':
+        if cs == 0:
+            pass
+        else:
+            del S[cs-1]
+            cs -= 1
+    if command[0] == 'P':
+        S.insert(cs, command[1])
+        cs += 1
+
+S = ''.join(S) 
+sys.stdout.write(S)
