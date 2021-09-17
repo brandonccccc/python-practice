af96c5196af3acfae8e9a71991d4d856e0bb5b43
brandonccccc <byeongjin180@gmail.com>
2021-09-17 09:44:52 +0900

    2004

--- 백준/연습장.py
+++ 백준/연습장.py
@@ -1,21 +1,2 @@
-h, w = map(int, input().split())
-n = int(input())
-a = [[[0] * w] * h]
-
-
-for i in range(n) :
-    l, d, x, y = map(int, input().split())
-    x -= 1
-    y -= 1
-    for j in range(y, y+l) :
-        if d == 0:
-            a[x][j] = 1
-    for j in range(x, x+l):
-        if d == 1:
-            a[j][y] = 1
-
-                
-for i in range(h):
-    for j in range(w):
-        print(a[i][j], end=' ')
-    print()
+a = str(12300)
+print(a[-1])
