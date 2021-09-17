f6ed3faa06957820124424bff68eeefc9785dc7c
brandonccccc <byeongjin180@gmail.com>
2021-09-17 13:46:19 +0900

    add 2004

--- 백준/2004_조합 0의 개수.py
+++ 백준/2004_조합 0의 개수.py
@@ -1,2 +1,18 @@
 n, m = map(int, input().split())
 
+def count_two(n):
+    two = 0
+    while n != 0:
+        n //= 2
+        two += n
+    return two
+
+def count_five(n):
+    five = 0
+    while n != 0:
+        n //=5
+        five += n
+    return five
+
+zero = min(count_two(n) - count_two(n - m) - count_two(m), count_five(n) - count_five(n - m) - count_five(m))
+print(zero)
