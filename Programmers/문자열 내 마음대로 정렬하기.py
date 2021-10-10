1e17ce519752a916962ad4dde1601a557bef5128
brandonccccc <byeongjin180@gmail.com>
2021-10-10 22:23:25 +0900

    lambda의 효율적인 사용? Stack overflow?

--- Programmers/문자열 내 마음대로 정렬하기.py
+++ Programmers/문자열 내 마음대로 정렬하기.py
@@ -1,3 +1,6 @@
 def solution(strings, n):
-    answer = []
-    return answer
+    return sorted(sorted(strings), key = lambda x: (x[n],x))
+strings = ["sun", "bed", "car"]
+print(solution(strings,1))
+
+#lambda의 효율적인 사용? Stack overflow?
