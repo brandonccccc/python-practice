dd16b1f3a01b6ebafc7f427e4cb034dd66ee9577
brandonccccc <byeongjin180@gmail.com>
2021-10-12 11:31:54 +0900

    비밀지도 비트연산

--- Programmers/비밀지도.py
+++ Programmers/비밀지도.py
@@ -15,7 +15,7 @@ def solution(n, arr1, arr2):
         
     return answer
 
-
+"""
 arr1 = [9, 20, 28, 18, 11]
 arr2 = [30, 1, 21, 17, 28]
 n = 5
@@ -36,4 +36,30 @@ for i in range(len(answer)):
 
 print(arr1)
 print(arr2)
+print(answer)
+"""
+"""
+def solution2(n, arr1, arr2):
+    answer = []
+    for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:])
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
+    return answer
+"""
+
+arr1 = [9, 20, 28, 18, 11]
+arr2 = [30, 1, 21, 17, 28]
+n = 5
+answer = []
+
+for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:]) #[2:] 첫자리 부호, 둘째자리 진법 표시부분 생략
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
+        
 print(answer)
