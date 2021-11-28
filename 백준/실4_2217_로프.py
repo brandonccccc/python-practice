85d0ac0827c8936c3d6a47b74c6a8d90a9680b40
brandonccccc <byeongjin180@gmail.com>
2021-11-28 22:58:37 +0900

    백준 실4 문제 풀이

--- 백준/실4_2217_로프.py
+++ 백준/실4_2217_로프.py
@@ -3,6 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 lopes = []
+answer = []
 for _ in range(n):
     lopes.append(int(input()))
 
+lopes.sort()
+for i in range(n):
+    answer.append(lopes[i]*(n-i))
+
+print(max(answer))
