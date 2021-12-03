f7a9502c5a4611093bf14e8357c7342e97a6fd5f
brandonccccc <byeongjin180@gmail.com>
2021-12-03 12:37:40 +0900

    프로그래머스 lev2

---
+++ Programmers/lev2_문자열압축.py
@@ -0,0 +1,32 @@
+def solution(s):
+    answer = []
+    if len(s) == 1:
+        return 1
+    for i in range(1, len(s)//2+1):
+        b = ''
+        cnt = 1
+        tmp = s[:i]
+        for j in range(i, len(s), i):
+            if tmp == s[j:i+j]:
+                cnt += 1
+            else:
+                if cnt != 1:
+                    b = b+str(cnt)+tmp
+                else:
+                    b = b+tmp
+                tmp = s[j:j+i]
+                cnt = 1
+        if cnt != 1:
+            b = b+str(cnt)+tmp
+        else:
+            b = b+tmp
+        
+        answer.append(len(b))
+    return min(answer)
+
+
+print(solution("aabbaccc"))
+print(solution("ababcdcdababcdcd"))
+print(solution("abcabcdede"))
+print(solution("abcabcabcabcdededededede"))
+print(solution("xababcdcdababcdcd"))
