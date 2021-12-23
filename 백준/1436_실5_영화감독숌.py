d10597bfee3bdde7f02cd5ce26c5243187846db0
brandonccccc <byeongjin180@gmail.com>
2021-12-23 20:55:54 +0900

    백준 문제 푸ㄹ기

---
+++ 백준/1436_실5_영화감독숌.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0 
+six = 666
+while True:
+    if '666' in str(six):
+        cnt += 1
+    if cnt == n:
+        print(six)
+        break
+    six += 1
