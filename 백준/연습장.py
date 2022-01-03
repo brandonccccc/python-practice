dbaa703b8ae093f2eaa278737665c7111d7c2384
brandonccccc <byeongjin180@gmail.com>
2022-01-03 22:37:16 +0900

    GUI 연습

--- 백준/연습장.py
+++ 백준/연습장.py
@@ -1,3 +1,15 @@
-list1 = []
-list1.append((str(1), 2))
-print(list1)
+q = input()
+print(q)
+print(type(q))
+
+print(*q)
+
+from collections import deque
+q = [1,2,3,4]
+
+def reverse_list(array):
+    if len(array) <= 1:
+        return array
+    return array[-1:] + reverse_list(array[:-1])
+
+print(reverse_list(q))
