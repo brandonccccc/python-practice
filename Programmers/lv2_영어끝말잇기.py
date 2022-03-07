b0eab67d083f82634c9f0c5d50a7f2f50b568ca1
brandonccccc <byeongjin180@gmail.com>
2022-03-07 12:58:27 +0900

    프로그래머스 Lv2

---
+++ Programmers/lv2_영어끝말잇기.py
@@ -0,0 +1,10 @@
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i-1]:
+            return [(i%n)+1, (i//n)+1]
+    else: return [0, 0]
+    
+    
+print(solution(3,["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+print(solution(5,["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+print(solution(2,["hello", "one", "even", "never", "now", "world", "draw"]))
